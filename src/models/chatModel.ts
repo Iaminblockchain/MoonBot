d7a1a92f92cc5621935e29fe43092d99a2f1afda
Ben <benjyc@proton.me>
2025-04-11 08:07:49 +0200

    Update Chat schema

--- src/models/chatModel.ts
+++ src/models/chatModel.ts
@@ -9,29 +9,12 @@ export interface IChat extends Document {
     message_count: { type: Number, default: 0 }
     token_count: { type: Number, default: 0 },
 }
-
 const ChatSchema: Schema = new Schema({
     chat_id: { type: String, required: true, unique: true },
+    username: { type: String, default: null },
+    message_count: { type: Number, default: 0 },
+    token_count: { type: Number, default: 0 },
     creation_date: { type: Date, default: Date.now },
 });
 
 export const Chat = mongoose.model<IChat>('Chat', ChatSchema);
-
-
-// ChatStatistics model
-// export interface IChatStatistics extends Document {
-//     creation: Date;
-//     update: Date;
-//     id: string;
-//     tokencount: number;
-// }
-
-// const ChatStatisticsSchema: Schema = new Schema({
-//     creation: { type: Date, default: Date.now },
-//     update: { type: Date, default: Date.now },
-//     id: { type: String, required: true, unique: true },
-//     tokencount: { type: Number, default: 0 },
-//     message_count: { type: Number, default: 0 }
-// });
-
-// export const ChatStatistics = mongoose.model<IChatStatistics>('ChatStatistics', ChatStatisticsSchema);
