a9b7fc702ba1516d07f9d489b4582c104c1b75e2
Patrick <ptrkstr@users.noreply.github.com>
2025-04-26 13:45:19 +0400

    🐛 Fix startup crash

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,10 @@
-import { logger } from "./logger";
 import * as dotenv from "dotenv";
 dotenv.config();
 
 export const retrieveEnvVariable = (variableName: string) => {
   const variable = process.env[variableName] || '';
   if (!variable) {
-    logger.info(`${variableName} is not set`);
+    console.log(`${variableName} is not set`);
     process.exit(1);
   }
   return variable;
