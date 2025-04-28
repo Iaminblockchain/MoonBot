3e19e4bedf6188d5cffe69c5a74772eebad95929
Patrick <ptrkstr@users.noreply.github.com>
2025-04-28 19:56:25 +0400

    🔥 Remove connection restart

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -5,37 +5,6 @@ import { TELEGRAM_STRING_SESSION, TELEGRAM_API_ID, TELEGRAM_API_HASH } from "../
 import { logger } from '../logger';
 import { processMessages } from "./processMessages";
 
-// https://trello.com/c/4VaLJ7N8
-// There are some circumstances that messages from telegram aren't emitted to us and we require manually polling.
-// This has been reported multiple times:
-// https://github.com/gram-js/gramjs/issues/575
-// https://github.com/gram-js/gramjs/issues/280
-// https://github.com/gram-js/gramjs/issues/654
-// https://github.com/gram-js/gramjs/issues/561
-// https://github.com/gram-js/gramjs/issues/494
-// https://github.com/gram-js/gramjs/issues/682
-// https://github.com/LonamiWebs/Telethon/issues/4345
-// Telegram has written a guide on it here: https://core.telegram.org/api/updates
-// Other users have suggested the following:
-// - One user from one of the above issues said they had success with calling getDialogs (https://github.com/gram-js/gramjs/issues/654#issuecomment-2029487203) periodically
-// - Another said calling getMe (https://github.com/gram-js/gramjs/issues/494#issuecomment-1593398280) periodically worked
-// 
-// We will try reconnecting every 25 minutes
-// https://github.com/gram-js/gramjs/issues/682#issuecomment-2169838423
-function restartConnectionPeriodically(client: TelegramClient): void {
-  const minutes = 30;
-  logger.info(`Will reconnect to Telegram every ${minutes} minutes`);
-  setInterval(async () => {
-    logger.info(`${minutes} minutes passed, reconnecting to Telegram...`);
-    try {
-      await client.getMe();
-      logger.info(`Did reconnect to Telegram`);
-    } catch (error) {
-      logger.error(`Error reconnecting to Telegram: ${error}`);
-    }
-  }, 1000 * 60 * minutes);
-}
-
 async function listenChats(client: TelegramClient): Promise<void> {
   client.addEventHandler(
     processMessages,
@@ -70,7 +39,6 @@ export async function scrape(client: TelegramClient): Promise<void> {
   logger.info("Start monitor and scrape chats");
   try {
     await listenChats(client);
-    restartConnectionPeriodically(client);
   } catch (e) {
     logger.error(`Error starting client: ${e}`);
   }
